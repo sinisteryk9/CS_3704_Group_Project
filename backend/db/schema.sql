7db6f0b0db94b7f7c9234b068265c383c631b5d9
trisha232024F <trisha23@vt.edu>
2026-04-15 15:32:58 -0400

    Added artifacts

--- backend/db/schema.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE accounts (
-  id SERIAL PRIMARY KEY,
-  email TEXT UNIQUE NOT NULL,
-  password_hash TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW()
-);
-
-
--- backend/db/seed.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO accounts (email, password_hash)
-VALUES
-  ('jeremy@example.com', 'Hash1'),
-  ('alice@gmail.com', 'Hash2'),
-  ('bob@icloud.com', 'Hash3');
